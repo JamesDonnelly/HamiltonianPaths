a8492ce4f0b0bb89d002891b9978fbf0c529850e
James Donnelly <hello@jamesmdonnelly.com>
2013-10-07 22:06:12 +0100

    Initial commit
    I'm not actually sure if every auto-generated pattern is completeable,
    but I guess I'll cross that path if someone raises it as an issue.

---
+++ Test.js
@@ -0,0 +1,8 @@
+$(function() {
+    /* Create the puzzles */
+    $('div#puzzle1').createPuzzle(5, 6);
+    $('div#puzzle2').createPuzzle(8, 6);
+    $('div#puzzle3').createPuzzle(8, 10);
+    $('div#puzzle4').createPuzzle(12, 10);
+    $('div#puzzle5').createPuzzle(12, 16);
+});
